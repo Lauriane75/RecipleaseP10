4dbf9bff6a7c0e5d6d8abeb22a4b73df94733f43
lauriane75 <lauriane.haydari@gmail.com>
2019-11-13 14:09:30 +0100

    Add project

---
+++ Reciplease/Reciplease/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reciplease
+//
+//  Created by Lauriane Haydari on 13/11/2019.
+//  Copyright © 2019 Lauriane Haydari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
